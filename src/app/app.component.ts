f0255511c09f3949f8376cf6449a95a8df45c624
Francisco Torres <francistorresmx@gmail.com>
2021-09-04 23:59:40 -0500

    SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser'; //para el titulo y meta para el SEO de google
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  title = 'FrancistorresMx';
+
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title); //el titulo
+    this.metaService.addTags([ // tarjetas del meta para SEO
+      {name: 'description', content: 'Hola, mi nombre es Francisco, actualmente soy estudiante de ingenieria en gestión y desarrollo de software. En esta página podras ver los proyectos en los que he trabajado.'},
+      {name: 'Author', content: 'FrancisTorresMex'},
+      {name: 'Title', content: 'FrancistorresMx'},
+      {name: 'keywords', content: 'Angular, Desarrollo, Portafolio, Programador, Codigo'},
+    ]);    
+  }
 }
